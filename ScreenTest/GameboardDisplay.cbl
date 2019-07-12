61ac73c65a034d24ce7e7f9590e1770f80798c1e
Obsol <33932119+read-0nly@users.noreply.github.com>
2019-07-12 17:35:28 -0400

    Got the whole pointer layer thing working!
    viva reddit!
    https://www.reddit.com/r/cobol/comments/ccbypw/help_request_starting_out_cobol_question_about/

---
+++ ScreenTest/GameboardDisplay.cbl
@@ -0,0 +1,122 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. GameboardDisplay.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+       01 black   constant as 0.
+       01 blue    constant as 1.
+       01 green   constant as 2.
+       01 cyan    constant as 3.
+       01 red     constant as 4.
+       01 magenta constant as 5.
+       01 brown   constant as 6.
+       01 white   constant as 7.
+       01 acpt-key         PIC X    VALUE SPACES.
+       01 pntr usage pointer.
+       01 player.
+           05 playerX pic 9(2) value 3.
+           05 playerY pic 9(2) value 3.
+           05 playerHealth pic 9(3) value 100.
+       01 width constant as 20.
+       01 height constant as 10.
+
+       01 godEye.
+           05 godEyeX pic 9(3) value 1.
+           05 godEyeY pic 9(3) value 1.
+           05 godMaxX pic 9(3).
+           05 godMaxY pic 9(3).
+
+       01 glyphs.
+           05 glyphWall pic x value "#".
+           05 glyphFloor pic x value "+".
+           05 glyphPlayer pic x value "U".
+           05 glyphSlime pic x value "o".
+           05 glyphVoid pic x value "@".
+       01 firstRow OCCURS height TIMES.
+           05 firstCol usage POINTER value null occurs width times.
+       01 secondRow OCCURS height TIMES.
+           05 secondCol  usage POINTER value null occurs width times.
+       01 thirdRow OCCURS height TIMES.
+           05 thirdCol usage POINTER value null occurs width times.
+       01 gameboardRow OCCURS height TIMES.
+           05 gameboardCol pic x value " " occurs width times.
+
+       LINKAGE SECTION.
+       01 godChar pic x(1) value null.
+       01 RowCurrent.
+           05 RowCurrentColumn pic x value " " occurs width times.
+       01 ColCurrent pic x.
+       SCREEN SECTION.
+       01 gameMap.
+          05 LINE 3 COL 25 PIC  X(20) FROM gameboardRow(1).
+          05 LINE 4 COL 25 PIC  X(20) FROM gameboardRow(2).
+          05 LINE 5 COL 25 PIC  X(20) FROM gameboardRow(3).
+          05 LINE 6 COL 25 PIC  X(20) FROM gameboardRow(4).
+          05 LINE 7 COL 25 PIC  X(20) FROM gameboardRow(5).
+          05 LINE 8 COL 25 PIC  X(20) FROM gameboardRow(6).
+          05 LINE 9 COL 25 PIC  X(20) FROM gameboardRow(7).
+          05 LINE 10 COL 25 PIC  X(20) FROM gameboardRow(8).
+          05 LINE 11 COL 25 PIC  X(20) FROM gameboardRow(9).
+          05 LINE 12 COL 25 PIC  X(20) FROM gameboardRow(10).
+
+       PROCEDURE DIVISION.
+       initialization.
+        add 1 to height giving godMaxX
+        add 1 to width giving godMaxY
+        PERFORM prepareMap
+       .
+       MAIN-PROCEDURE.
+        PERFORM drawMap
+        DISPLAY gameMap
+        ACCEPT ACPT-KEY TIMEOUT AFTER 10 with auto
+        move null to secondCol(5,5)
+        GO TO MAIN-PROCEDURE
+       .
+       drawMap.
+         move 1 to godEyeX
+         move 1 to godEyeY
+         PERFORM until godEyeX = godMaxX
+          PERFORM until godEyeY = godMaxY
+           if firstCol(godEyeX,godEyeY) = NULL then
+            if secondCol(godEyeX,godEyeY) = NULL then
+             if thirdCol(godEyeX,godEyeY) = NULL then
+             ELSE
+              SET ADDRESS OF godChar to thirdCol(godEyeX,godEyeY)
+              move godChar to gameboardCol(godEyeX,godEyeY)
+             END-IF
+            ELSE
+             SET ADDRESS OF godChar to secondCol(godEyeX,godEyeY)
+             move godChar to gameboardCol(godEyeX,godEyeY)
+            END-IF
+           ELSE
+             SET ADDRESS OF godChar to firstCol(godEyeX,godEyeY)
+             move godChar to gameboardCol(godEyeX,godEyeY)
+           end-if
+           add 1 to godEyeY
+          END-PERFORM
+          move 1 to godEyeY
+          add 1 to godEyeX
+         END-PERFORM
+         move 1 to godEyeX
+         move 1 to godEyeY
+         set address of godChar to NULL
+       .
+       prepareMap.
+         move address of glyphWall to secondCol(1,1)
+         move address of glyphPlayer to secondCol(3,3)
+         move address of glyphSlime to secondCol(5,5)
+         move address of glyphFloor to thirdCol(5,5)
+         move address of glyphFloor to thirdCol(6,6)
+         move address of glyphFloor to thirdCol(7,7)
+
+       .
+       ENDGAME.
+        STOP RUN.
+        END PROGRAM GameboardDisplay.
